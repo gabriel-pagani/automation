0b7808d7b3d710cb2bd21fc55ba7acb673dadf65
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-06-10 14:48:31 -0300

    Criei um módulo para a conexão com o banco de dados

---
+++ server/connection.py
@@ -0,0 +1,44 @@
+from pyodbc import connect, Error
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+_connection = None
+
+
+def get_connection() -> connect:
+    global _connection
+    if _connection is None or _connection.closed:
+        server_connection = f'DRIVER={{SQL Server}}; SERVER={getenv("SERVER")}; DATABASE={getenv("DATABASE")}; UID={getenv("USER")}; PWD={getenv("PASSWORD")}'
+        try:
+            _connection = connect(server_connection)
+        except Error as e:
+            print(f"Connection error: {e}")
+            raise
+    return _connection
+
+
+def close_connection() -> None:
+    global _connection
+    if _connection and not _connection.closed:
+        _connection.close()
+        _connection = None
+
+
+def server_request(query: str) -> dict:
+    try:
+        connection = get_connection()
+        with connection.cursor() as cursor:
+
+            cursor.execute(query)
+            columns = [column[0] for column in cursor.description]
+            data = cursor.fetchall()
+            response = []
+            for row in data:
+                response.append(dict(zip(columns, row)))
+
+    except Exception as e:
+        print(f"Request error: {e}")
+
+    return response[0]
