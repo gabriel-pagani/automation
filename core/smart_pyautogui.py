dd1b3c7f846319914c177db29d13736988927e21
Gabriel Pagani de Souza <gabriel.souza@gruposmi.com.br>
2025-09-26 15:31:38 -0300

    Melhorei as funções de clique inteligentes

--- core/smart_pyautogui.py
+++ core/smart_pyautogui.py
@@ -1,29 +1,29 @@
 import pyautogui
 
 
-def smart_click(x: int = None, y: int = None, image_path: str = None, flag_path: str = None, confidence: int = 0.90):
+def smart_click(x: int = None, y: int = None, image_path: str = None, flag_path: str = None, clicks: int = 1, confidence: int = 0.90):
     while True:
         try:
             if x is not None and y is not None and not flag_path:
-                pyautogui.click(x=x, y=y)
+                pyautogui.click(x=x, y=y, clicks=clicks)
                 break
 
             if x is not None and y is not None and flag_path:
                 if pyautogui.locateOnScreen(flag_path, confidence=confidence):
-                    pyautogui.click(x=x, y=y)
+                    pyautogui.click(x=x, y=y, clicks=clicks)
                     break
 
             if image_path and not flag_path:
                 location = pyautogui.locateOnScreen(image_path, confidence=confidence)
                 if location:
-                    pyautogui.click(pyautogui.center(location))
+                    pyautogui.click(pyautogui.center(location), clicks=clicks)
                     break
 
             if image_path and flag_path:
                 if pyautogui.locateOnScreen(flag_path, confidence=confidence):
                     location = pyautogui.locateOnScreen(image_path, confidence=confidence)
                     if location:
-                        pyautogui.click(pyautogui.center(location))
+                        pyautogui.click(pyautogui.center(location), clicks=clicks)
                         break
 
         except Exception:
